b4b3416c5f80bc222af0333b9f64171d78f58edd
Yuliya Nikiforova <y.sokolova.liya@gmail.com>
2023-02-26 18:57:10 +0200

    add margin-top

--- src/components/CharContent/CharContentStyled.js
+++ src/components/CharContent/CharContentStyled.js
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 
 const CharContentStyled = styled.div`
-  margin-top: 50px;
+  margin: 50px 0;
   display: grid;
   grid-template-columns: 650px 425px;
   column-gap: 25px;
